252b7fecc49271125ad5999854847093066bc8a0
mlateulade <77686194+mlateulade@users.noreply.github.com>
2021-06-25 02:45:28 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,16 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+
+    Console.WriteLine("Enter salesperson's Name");
+    string name = Console.ReadLine();
+    Console.WriteLine("Enter sales amount");
+    var amount = Console.ReadLine();
+
+    
+
+
+
+  }
+}
